4b7aff003ae0af07d7417bc94784ec0270e8c85f
montanizstills <montaniz@student.fdu.edu>
2020-02-26 11:36:20 -0500

    infering sentiment

--- src/main/java/com/github/nez/ml/MachineLearning.java
+++ src/main/java/com/github/nez/ml/MachineLearning.java
@@ -4,4 +4,19 @@ package com.github.nez.ml;
 
 public class MachineLearning {
 //Deep nueral networks, find trained models
+
+        void hasAnomaliyOccured(){
+            boolean war = false;
+            //search for instance of the word war
+//             -- sources, time, who is in the war,
+//            filter?? weight insignificant countries and nations
+            boolean naturalDisaster = false;
+            boolean plague = false;
+            boolean disruptionInGlobalSupplyChain = false;
+
+            if(war || naturalDisaster || plague || disruptionInGlobalSupplyChain ){
+
+            }
+    // war, plague, natural disaster
+    }
 }
--- src/main/java/com/github/nez/nlp/SentimentAnalysis.java
+++ src/main/java/com/github/nez/nlp/SentimentAnalysis.java
@@ -10,12 +10,14 @@ public class SentimentAnalysis {
     public static void main(String[] args) {
         StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
         String text= "Ok, so maybe I am feeling a little guilty for not studying as hard as I should have at Perscholas. But I will give all my praise to God for He has brought me through -- and over high mountains!";
-        CoreDocument coreDocument = new CoreDocument(text);
+        String text2 = "America is involved in a war and is losing";
+        CoreDocument coreDocument = new CoreDocument(text2);
         stanfordCoreNLP.annotate(coreDocument);
         List<CoreSentence> coreSentenceList = coreDocument.sentences();
         for(CoreSentence sentence: coreSentenceList){
             String sentiment = sentence.sentiment();
             System.out.println(sentiment+"\t"+sentence);
         }
+        // infering sentiments
     }
 }
